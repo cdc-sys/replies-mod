ce32b0aa123b7932fdbc452b67ceed59c9da0dbc
cdc-sys <mzubkov574@gmail.com>
2025-04-20 02:36:11 +0200

    add server down notice

--- src/Structs.hpp
+++ src/Structs.hpp
@@ -165,4 +165,4 @@ inline std::string toAgoString(int timestamp) {
     return fmt::format("this is the secret string");
 }
 static constexpr const std::string_view SERVER_URL = "https://tf7lj3c8-6650.euw.devtunnels.ms";
-static const std::string MOD_VERSION_HEADER = "testing.2";
+static const std::string MOD_VERSION_HEADER = "testing.3";
