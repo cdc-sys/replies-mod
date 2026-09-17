a17554befc08a7f54f6d9e8740d122c23d243be2
cdc-sys <mzubkov574@gmail.com>
2026-09-18 01:04:53 +0200

    fix button position

--- src/main.cpp
+++ src/main.cpp
@@ -6,24 +6,42 @@ using namespace geode::prelude;
 
 #include <Geode/modify/CommentCell.hpp>
 class $modify(MyCommentCell, CommentCell) {
+	struct Fields {
+		bool replyButtonAdded = false;
+	};
+	static void onModify(auto& self) {
+		(void) self.setHookPriority("CommentCell::loadFromComment", -4000); // using -3999 int prio because im doing stuff before AND after a function call and an enum would make it unclear. also -2123456789 prio on top
+	}
 	void loadFromComment(GJComment* comment){
 		CommentCell::loadFromComment(comment);
+		if (m_fields->replyButtonAdded) return;
+		m_fields->replyButtonAdded = true;
+
 		bool smallCommentsMode = this->m_height == 36;
+		auto eryMenu = this->getChildByIDRecursive("raydeeux.variouscommenttweaks/the-menu-with-buttons-that-im-too-lazy-to-move-into-main-menu-sorry");
+		auto dateLabel = this->m_mainLayer->getChildByID("date-label");
+
 		auto replyLabel = CCSprite::createWithSpriteFrameName("GJ_undoBtn_001.png");
 		auto replyButton = CCMenuItemExt::createSpriteExtra(replyLabel, [comment](CCMenuItemSpriteExtra* btn){
 			auto replyLayer = ReplyLayer::create(comment);
 			replyLayer->show();
 		});
 		auto replyMenu = CCMenu::create();
+
 		if (!smallCommentsMode){
 			replyMenu->setPosition(320,30);
 			replyLabel->setScale(.45f);
 		}
 		else{
-			replyMenu->setPosition(297,8);
+			replyMenu->setPosition(dateLabel->getPositionX()-dateLabel->getScaledContentWidth()-10.f,8);
+			if (eryMenu) {
+				geode::log::info("test");
+				replyMenu->setPosition(eryMenu->getPositionX()-eryMenu->getScaledContentWidth()-5.f,8);
+			}
 			replyLabel->setScale(.30f);
 		}
+
 		replyMenu->addChild(replyButton);
-		this->addChild(replyMenu);
+		this->m_mainLayer->addChild(replyMenu);
 	}
 };
